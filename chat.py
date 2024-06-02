4c4d28c6f5f3d3194142de80b4b89eb95dadfbe4
salman <salman.ahmad-tajudin@basf.com>
2024-06-03 02:32:27 +0800

    first commit

---
+++ chat.py
@@ -0,0 +1,60 @@
+import os.path
+
+# import openai
+from dotenv import load_dotenv
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+load_dotenv()
+
+# openai.api_key = os.getenv["OPENAI_API_KEY"]
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("./data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+
+if os.path.exists(PERSIST_DIR):
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+
+# if not os.path.exists(PERSIST_DIR):
+#     # load the documents and create the index
+#     documents = SimpleDirectoryReader("data").load_data()
+#     index = VectorStoreIndex.from_documents(documents)
+#     # store it for later
+#     index.storage_context.persist(persist_dir=PERSIST_DIR)
+# else:
+#     # load the existing index
+#     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+#     index = load_index_from_storage(storage_context)
+
+# Either way we can now query the index
+chat_engine = index.as_chat_engine(
+    chat_mode="context",
+    system_prompt=(
+        "You are a chat assistant that helps users understand this article. Just answer based on the context of this article."
+    ),
+)
+response = chat_engine.chat("hi")
+print(response)
+
+response = chat_engine.chat("who is first Malaysian prime minister?")
+print(response)
+
+response = chat_engine.chat("explain point 1 and 10")
+print(response)
+
+response = chat_engine.chat("apakah point ke-1 dan ke-10?")
+print(response)
