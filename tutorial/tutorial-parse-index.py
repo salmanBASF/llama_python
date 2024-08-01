c4669e8ca53e98a0cf0681ffe527e17c0c1e3ede
salman <salman.ahmad-tajudin@basf.com>
2024-08-01 21:35:44 +0800

    chore: improe logic storage

--- tutorial/tutorial-parse-index.py
+++ tutorial/tutorial-parse-index.py
@@ -14,68 +14,84 @@ from llama_index.core import (
 # Load environment variables from .env file
 load_dotenv()
 
-# ---------------- LLAMA CLOUD : PARSING----------------#
+
+# ---------------- LLAMAINDEX : ENV KEYS----------------#
+
+# Read the API key from the environment variable
+api_key = os.getenv("OPENAI_API_KEY")
+
+if api_key is None:
+    # raises an error that needs to be handled by the calling code or by an exception handler.
+    raise ValueError("OPENAI_API_KEY variable not set in .env")
 
 # Read the API key from the environment variable
 LLAMA_CLOUD_API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
 
-parser = LlamaParse(
-    api_key=LLAMA_CLOUD_API_KEY,  # can also be set in your env as LLAMA_CLOUD_API_KEY
-    result_type="markdown",  # "markdown" and "text" are available
-    parsing_instruction="Extract main content. Remove navigation elements , sidebars, footers, and unrelated content. Set heading level 1 for the title of the content. There is only one heading level 1",  # parsing instructions
-    show_progress=True,  # show progress bar
-)
+
+# ---------------- LLAMA CLOUD : PARSING----------------#
 
 
 # Specify the filename for the HTML document to be parsed. Note: exclude the .html extension
-html_filename = "samsung_galaxy"
-
-# Load the HTML document using the parser's load_data method
-documents = parser.load_data(f"./storage_document/{html_filename}.html")
-
-# Extract the text content from the first document
-text_doc = documents[0].text
-
-# Print the extracted text
-print(text_doc)
+HTML_FILENAME = "meta_ai"
 
 # Specify the output file path for the parsed document. Note: md is file extension for markdown files
-parse_file_path = f"./storage_parse/{html_filename}.md"
+PARSE_FILE_PATH = f"./storage_parse/{HTML_FILENAME}.md"
 
-# Write the extracted text to the output file
-with open(parse_file_path, "w", encoding="utf-8") as output_file:
-    output_file.write(text_doc)
+# check if PARSE_FILE_PATH is exists,
+if os.path.exists(PARSE_FILE_PATH):
+    print(f"Parse Documents already exist in {PARSE_FILE_PATH}")
+# if not exists, parse the document
+else:
+    # Create an instance of the LlamaParse class
+    parser = LlamaParse(
+        api_key=LLAMA_CLOUD_API_KEY,  # can also be set in your env as LLAMA_CLOUD_API_KEY
+        result_type="markdown",  # "markdown" and "text" are available
+        parsing_instruction="Extract main content. Remove navigation elements , sidebars, footers, and unrelated content. Set heading level 1 for the title of the content. There is only one heading level 1",  # parsing instructions
+    )
 
-# Print a message indicating the successful download and save of the parsed document
-print(f"Documents downloaded and saved to {parse_file_path}")
+    # Load the HTML document using the parser's load_data method
+    documents = parser.load_data(f"./storage_document/{HTML_FILENAME}.html")
+
+    # Extract the text content from the first document
+    text_doc = documents[0].text
+
+    # Print the extracted text
+    print(text_doc)
+
+    # Write the extracted text to the output file
+    with open(PARSE_FILE_PATH, "w", encoding="utf-8") as output_file:
+        output_file.write(text_doc)
+
+    # Print a message indicating the successful download and save of the parsed document
+    print(f"Documents downloaded and saved to {PARSE_FILE_PATH}")
 
 
 # ---------------- LLAMA INDEX : INDEXING----------------#
 
 
 # generate the folder named storage_index/{filename} automatically
-STORAGE_INDEX_DIR = f"./storage_index/{html_filename}"
+STORAGE_INDEX_DIR = f"./storage_index/{HTML_FILENAME}"
 
-# check the index storage directory not exists, create the new indwx
-if not os.path.exists(STORAGE_INDEX_DIR):
-    # TODO load the data from the parse_file_path
-    parse_documents = SimpleDirectoryReader(input_files=[parse_file_path]).load_data()
-    # create the index
-    index = VectorStoreIndex.from_documents(parse_documents)
-    # store the index in the storage_index directory
-    index.storage_context.persist(persist_dir=STORAGE_INDEX_DIR)
 
 # if the index storage directory exists, load the existing index
 if os.path.exists(STORAGE_INDEX_DIR):
     storage_context = StorageContext.from_defaults(persist_dir=STORAGE_INDEX_DIR)
     index = load_index_from_storage(storage_context)
 
+# if the index storage directory not exists, create the new index
+else:
+    # load the data from the PARSE_FILE_PATH
+    parse_documents = SimpleDirectoryReader(input_files=[PARSE_FILE_PATH]).load_data()
+    # create the index
+    index = VectorStoreIndex.from_documents(parse_documents)
+    # store the index in the storage_index directory
+    index.storage_context.persist(persist_dir=STORAGE_INDEX_DIR)
 
 # index object is being used to create a query engine for searching and retrieving information from the index
 query_engine = index.as_query_engine()
 
 # You can query any information you want from the document you uploaded
-response = query_engine.query("What is the document is about?")
+response = query_engine.query("What is news about Llama 4?")
 
 # print the response to the console
 print(response)
